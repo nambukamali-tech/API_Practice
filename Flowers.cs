9d0c133ef39fab3a79535a6d7afe2ddb2201d84c
Nambu Kamali <nambukamali@gmail.com>
2026-02-26 11:24:42 +0530

    Add files via upload

---
+++ Flowers.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Flowers_API.Models
+{
+    public class Flowers
+    {
+        [Key]
+        public int FlowerId { get; set; }
+        public string FlowerName { get; set; } = string.Empty;
+        public string FlowerColor { get; set; } = string.Empty;
+        public string FlowerSeason { get; set; } = string.Empty;
+        public string FlowerArea { get; set; } = string.Empty;
+
+
+    }
+}
