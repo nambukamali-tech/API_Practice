4c979790932e7772808b6ee9f1626544987e15f9
Nambu Kamali <nambukamali@gmail.com>
2026-02-26 11:24:20 +0530

    Model of Flowers API

---
+++ FlowerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Flowers_API.Models;
+namespace Flowers_API.Data
+{
+    public class FlowerDbContext : DbContext
+    {
+        public FlowerDbContext(DbContextOptions<FlowerDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Flowers> Flowers { get; set; }
+
+    }
+}
