146dc7883ad0d52977b7ce7ed5348916fac0dbec
Nambu Kamali <nambukamali@gmail.com>
2026-02-27 21:33:55 +0530

    Add files via upload

---
+++ RegisterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoo_API.Models
+{
+    public class RegisterRequest
+    {
+        [Required(ErrorMessage ="UserName is Required")]
+        [StringLength(50)]
+        public string UserName { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Password is Required")]
+        [StringLength(60)]
+        public string Password { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Email is Required")]
+        [StringLength(50)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
