2d0c08694ce43f428cbbfbb80cf2ddc987e01cdc
Nambu Kamali <nambukamali@gmail.com>
2026-02-27 21:33:37 +0530

    Add files via upload

---
+++ LoginRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoo_API.Models
+{
+    public class LoginRequest
+    {
+        [Required(ErrorMessage ="UserName is Required")]
+        [StringLength(50)]
+        public string UserName { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Password is Required")]
+        [StringLength(50)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
