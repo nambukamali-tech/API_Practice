00adbc4ee804b32cfdfdd555a7d94604e97182dd
Nambu Kamali <nambukamali@gmail.com>
2026-02-27 21:34:17 +0530

    Add files via upload

---
+++ Users.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Zoo_API.Models;
+namespace Zoo_API.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="UserName is Required")]
+        [StringLength(100)]
+
+        public string UserName { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Password is Required")]
+        [StringLength(50)]
+
+        public string Password { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Email Id is Required")]
+        [StringLength(40)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
